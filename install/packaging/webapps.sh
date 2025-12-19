a4341c03ad0bb91496f6561054122b8282399263
CodySwagor <cody@stratonimbus.org>
2025-12-19 06:38:31 +0000

    feat: Remove Discord Webapp

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,4 +1,3 @@
 omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
 omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
 omarchy-webapp-install "Figma" https://figma.com/ Figma.png
-omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
