a8950e199fb15a3c6d3570f7bbe6a27a1c2a483c
CodySwagor <cody@stratonimbus.org>
2025-12-19 06:40:49 +0000

    feat: Remove Spotify

--- migrations/1757877852.sh
+++ migrations/1757877852.sh
@@ -1,5 +1,4 @@
 echo "Switch select bindings to launch or focus mode"
 
 if [[ -f ~/.config/hypr/bindings.conf ]]; then
-  sed -i '/SUPER, M, Music, exec/ c\bindd = SUPER, M, Music, exec, omarchy-launch-or-focus spotify' ~/.config/hypr/bindings.conf
   sed -i '/SUPER, O, Obsidian, exec/ c\bindd = SUPER, O, Obsidian, exec, omarchy-launch-or-focus obsidian "uwsm-app -- obsidian -disable-gpu --enable-wayland-ime"' ~/.config/hypr/bindings.conf
