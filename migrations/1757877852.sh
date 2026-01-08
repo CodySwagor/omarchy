63a6dfb7ff5064199cd894cb87fa9f02dfb3ff59
CodySwagor <cody@stratonimbus.org>
2026-01-08 20:49:36 +0000

    feat: Remove Signal Webapp

--- migrations/1757877852.sh
+++ migrations/1757877852.sh
@@ -4,6 +4,5 @@ if [[ -f ~/.config/hypr/bindings.conf ]]; then
   sed -i '/SUPER, M, Music, exec/ c\bindd = SUPER, M, Music, exec, omarchy-launch-or-focus spotify' ~/.config/hypr/bindings.conf
   sed -i '/SUPER, O, Obsidian, exec/ c\bindd = SUPER, O, Obsidian, exec, omarchy-launch-or-focus obsidian "uwsm-app -- obsidian -disable-gpu --enable-wayland-ime"' ~/.config/hypr/bindings.conf
 
-  sed -i '/SUPER, G, Signal, exec/ c\bindd = SUPER, G, Signal, exec, omarchy-launch-or-focus signal "uwsm-app -- signal-desktop"' ~/.config/hypr/bindings.conf
   sed -i '/SUPER SHIFT, G, WhatsApp, exec/ c\bindd = SUPER SHIFT, G, WhatsApp, exec, omarchy-launch-or-focus-webapp WhatsApp "https://web.whatsapp.com/"' ~/.config/hypr/bindings.conf
 fi
